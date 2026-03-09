01a061a6ddd0bd1e6be465fbdfdbe05aaeba378a
SatrioHubs <satrioramadhani387@gmail.com>
2026-03-09 19:29:28 +0700

    doone JS 6

---
+++ code/jobsheet6/my-app/src/pages/404.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/404.module.scss';
+import Link from 'next/link';
+
+const Custom404 = () => {
+  return (
+    <div className={styles.error}>   
+        <h1>404 - Halaman Tidak Ditemukan</h1>
+        <img src="./page-not-found.png" alt="404" className={styles.image}/>
+        <p>maaf, Halaman yang Anda cari tidak ada.</p>
+        <Link href="/" className={styles.link}>Kembali ke Beranda</Link>
+    </div>
+  )
+}
+export default Custom404;
---
+++ code/jobsheet6/my-app/src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <head>
+          <title>Praktikum Next.JS Page Router</title>
+        </head>
+        <h1>Praktikum Next.JS Page Router</h1>
+        <p>Mahasiswa D4 Pengembangan Web</p>
+      </div>
+    </>
+  )
+}
